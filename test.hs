dbb3f3edaf7a039df9a6220ee4b16feb0c75ca44
powcoder <powcoder@163.com>
2022-12-08 08:50:14 +0800

    TriTree

---
+++ test.hs
@@ -0,0 +1,14 @@
+https://powcoder.com
+代写代考加微信 powcoder
+Assignment Project Exam Help
+Add WeChat powcoder
+https://powcoder.com
+代写代考加微信 powcoder
+Assignment Project Exam Help
+Add WeChat powcoder
+
+nodeValue (TriNode 10 EmptyNode EmptyNode EmptyNode)
+leftChild (TriNode 10 (TriNode 1 EmptyNode EmptyNode EmptyNode) EmptyNode EmptyNode) == (TriNode 1 EmptyNode EmptyNode EmptyNode)
+middleChild (TriNode 10  EmptyNode (TriNode 1 EmptyNode EmptyNode EmptyNode) EmptyNode) == (TriNode 1 EmptyNode EmptyNode EmptyNode)
+rightChild (TriNode 10  EmptyNode  EmptyNode (TriNode 1 EmptyNode EmptyNode EmptyNode)) == (TriNode 1 EmptyNode EmptyNode EmptyNode)
+
